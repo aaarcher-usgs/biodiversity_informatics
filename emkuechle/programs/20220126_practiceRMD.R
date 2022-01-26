4135471a6c08df43c5a1b8559a4d1c1974f38766
emkuechle <qo2233hr@go.minnstate.edu>
2022-01-26 15:41:44 -0600

    3rd level header

--- emkuechle/programs/20220126_practiceRMD.R
+++ emkuechle/programs/20220126_practiceRMD.R
@@ -26,6 +26,8 @@ remove(list = ls())
 #' 7. Create a third chunk of code that evaluates x/y when x = 500, y = 50
 #' 
 
+#' Work Here:
+#' ### This is my 3rd level header
 #' 
 #' Chunk 1: Here, we will evaluate x/y when x = 200, y = 50
 #' 
