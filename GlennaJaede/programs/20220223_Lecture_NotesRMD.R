87fdf77736d2df30cadf04a1ef2fa3aafa6aa3ae
GlennaJaede <gljaede@go.stcloudstate.edu>
2022-02-23 14:16:00 -0600

    Trying notes in r

---
+++ GlennaJaede/programs/20220223_Lecture_NotesRMD.R
@@ -0,0 +1,29 @@
+#' # Lecture Notes
+#' 
+#' Programmer: Glenna
+#' 
+#' Date: February 23, 2022
+#' 
+#' 
+#' ## Header
+#' 
+#' This chunk of code will load in all required packages and clear environment
+#' 
+#+ header
+library(knitr)
+library(ezknitr)
+# This line clears the environment
+remove(list = ls()) 
+
+## Notes
+
+
+
+#' To test (and finalize) your work, spin the document to html using this code:
+#' 
+#' ezspin(file = "GlennaJaede/programs/20220223_Lecture_NotesRMD.R",out_dir = "GlennaJaede/output",fig_dir = "figures",keep_md = FALSE)
+#' 
+#' **Be careful not to overwrite anyone else's output!!**
+#' 
+#' Remember to commit > pull > push to submit your work to GitHub. Do this 
+#' frequently!
