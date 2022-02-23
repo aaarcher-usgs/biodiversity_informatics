999e3557df08882c08d713b6ef5fd10a7b4ce896
hhelmb20 <hkhelmbrecht@stcloudstate.edu>
2022-02-23 16:00:55 -0600

    lab 5

--- HannahHelmbrecht/programs/20220223_range_data.R
+++ HannahHelmbrecht/programs/20220223_range_data.R
@@ -26,7 +26,7 @@ set.seed(71587)
 #' ### Use biocManager to get package GenomicRanges
 #' 
 # if (!require("BiocManager", quietly = TRUE))#   install.packages("BiocManager")# BiocManager::install(version = "3.14")# Now install the package
- BiocManager::install("GenomicRanges")# library(IRanges)
+# BiocManager::install("GenomicRanges")# library(IRanges)
 
 #' Source the script from the textbook to make figures
 #' 
