b9d4f8e8cc3f639d77034f812166671c42e82fd6
hb2244jm <hb2244jm@go.minnstate.edu>
2022-04-06 16:47:28 -0500

    changes

--- savannadroher/programs/20220406_example_SDM.R
+++ savannadroher/programs/20220406_example_SDM.R
@@ -11,6 +11,7 @@
 #' 
 # Load Libraries
 
+library(ezknitr)
 library(rgbif)
 library(terra)
 library(sdmpredictors)
@@ -290,7 +291,7 @@ m1
 
 #' Prediction map
 #' 
-p1 <- predict(m1, newdata = layers_cut, filename='aaarcher/output/figures/p1.img') 
+p1 <- predict(m1, newdata = layers_cut, filename='savannadroher/output/figures/p1.img') 
 plot(studyarea, border = "red", lwd = 3)
 plot(countries, border = "tan", add = T)
 plot(p1, add = T)
@@ -301,4 +302,4 @@ plot(p1, add = T)
 #' ### Footer
 #' 
 #' spin this with:
-#' ezspin(file = "aaarcher/programs/20220406_example_SDM.R",out_dir = "aaarcher/output", fig_dir = "figures",keep_md = FALSE, keep_rmd = FALSE)
+#' ezspin(file = "savannadroher/programs/20220406_example_SDM.R",out_dir = "savannadroher/output", fig_dir = "figures",keep_md = FALSE, keep_rmd = FALSE)
